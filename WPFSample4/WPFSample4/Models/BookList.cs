795979bf7a51238cce5a0d8191284374230fb548
Takayuki Nishikawa <tt8a02km21113@gmail.com>
2018-06-17 15:06:09 +0900

    初回コミット

---
+++ WPFSample4/WPFSample4/Models/BookList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSample4.Models
+{
+    public class BookList
+    {
+        /// <summary>
+        /// 書籍リストクラス
+        /// </summary>
+        private List<Book> books;
+
+        public BookList()
+        {
+            this.books = new List<Book>();
+        }
+
+        public BookList(List<Book> someList)
+        {
+            this.books = new List<Book>(someList);
+        }
+
+        public List<Book> GetList()
+        {
+            return new List<Book>(books);
+        }
+
+
+        public List<Book> Add(Book someBook)
+        {
+            books.Add(someBook);
+
+            return new List<Book>(books);
+        }
+
+        public List<Book> Del(Book someBook)
+        {
+            foreach(var book in books)
+            {
+                if(book.ID == someBook.ID)
+                {
+                    books.Remove(book);
+                    break;
+                }
+
+            }
+            return new List<Book>(books);
+        }
+
+        public void DelAll()
+        {
+            books.Clear();
+        }
+    }
+}
---
+++ WPFSample4/WPFSample4/ViewModels/MainViewModel.cs
@@ -0,0 +1,114 @@
+﻿
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using WPFSample4.Models;
+
+namespace WPFSample4.ViewModels
+{
+    /// <summary>
+    /// ViewModel用クラス
+    /// </summary>
+    public class MainViewModel : BindableBase
+    {
+        /// <summary>
+        /// 入力フィールド用の書籍インスタンス
+        /// </summary>
+        private Book _Book;
+        public Book Book
+        {
+            get { return _Book; }
+            set { this.SetProperty(ref this._Book, value); }
+        }
+
+        /// <summary>
+        /// 書籍リスト
+        /// </summary>
+        private BookList books;
+
+        /// <summary>
+        /// バインディング用コレクションリスト
+        /// </summary>
+        public ObservableCollection<Book> CollectionList { get; set; }
+
+        /// <summary>
+        /// 追加コマンドデリゲート
+        /// </summary>
+        private DelegateCommand _AddComamnd;
+        public DelegateCommand AddCommand
+        {
+            get { return _AddComamnd = _AddComamnd ?? new DelegateCommand(AddCommandExecute); }
+        }
+
+        /// <summary>
+        /// 削除コマンドデリゲート
+        /// </summary>
+        private DelegateCommand<Book> _DelComamnd;
+        public DelegateCommand<Book> DelComamnd
+        {
+            get { return _DelComamnd = _DelComamnd ?? new DelegateCommand<Book>(DelCommandExecute); }
+        }
+
+        /// <summary>
+        /// 全削除コマンドデリゲート
+        /// </summary>
+        private DelegateCommand _DelAllComamnd;
+        public DelegateCommand DelAllComamnd
+        {
+            get { return _DelAllComamnd = _DelAllComamnd ?? new DelegateCommand(DelAllCommandExecute); }
+        }
+
+        /// <summary>
+        /// 追加コマンドを実行する
+        /// </summary>
+        private void AddCommandExecute()
+        {
+            Book addBook = new Book(books.GetList().Count + 1, Book.Title, Book.Author, Book.Price);
+            books.Add(addBook);
+
+            CollectionList.Add(addBook);
+        }
+
+        /// <summary>
+        /// 削除コマンドを実行する
+        /// </summary>
+        /// <param name="someBook"></param>
+        private void DelCommandExecute(Book someBook)
+        {
+            books.Del(someBook);
+
+            CollectionList.Clear();
+
+            foreach(var book in books.GetList())
+            {
+                CollectionList.Add(book);
+            }
+        }
+
+        /// <summary>
+        /// 全削除コマンドを実行する
+        /// </summary>
+        /// <param name="someBook"></param>
+        private void DelAllCommandExecute()
+        {
+            books.DelAll();
+
+            CollectionList.Clear();
+        }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainViewModel()
+        {
+            Book = new Book();
+
+            books = new BookList();
+
+            books.Add(new Book() { ID = 1, Title = "簡単すぎる本", Author = "アンドロイド1号", Price = 100 });
+            books.Add(new Book() { ID = 2, Title = "普通すぎる本", Author = "アンドロイド2号", Price = 200 });
+            books.Add(new Book() { ID = 3, Title = "難しすぎる本", Author = "アンドロイド3号", Price = 300 });
+
+            CollectionList = new ObservableCollection<Book>(books.GetList());
+        }
+    }
+}
